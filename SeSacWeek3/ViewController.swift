4924d7289b2bde5c2704982a22065a7102aa8b34
jun <june23rd.kim@gmail.com>
2025-07-11 11:18:36 +0900

    주석으로 할일 목록 추가 및 print(0) 으로 수정

--- SeSacWeek3/ViewController.swift
+++ SeSacWeek3/ViewController.swift
@@ -11,7 +11,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print(0)
+        print(1)
         print(2)
         print(3)
         print(4)
@@ -23,6 +23,10 @@ class ViewController: UIViewController {
         for _ in 1...10 {
             print("반복문을 돌려봅시다")
         }
+        
+        // 오늘 할일 목록
+        // 테이블뷰 복습 및 커스텀셀로 만들어보기 연습
+        // (최대한 많이!!)
     }
 
 
