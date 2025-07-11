2c96ddbcc4fc20146f7c24bf5e318c5ef9ed588f
jun <june23rd.kim@gmail.com>
2025-07-11 11:06:16 +0900

    Initial Commit

---
+++ SeSacWeek3/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  SeSacWeek3
+//
+//  Created by 김현중 on 7/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(1)
+        print(2)
+        print(3)
+    }
+
+
+}
+
